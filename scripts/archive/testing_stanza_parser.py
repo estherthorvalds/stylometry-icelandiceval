bf634913c5a9f9298271b9b54b2d6321c598e718
esther <92090317+estherthorvalds@users.noreply.github.com>
2026-04-06 23:55:37 +0000

    research log and decision log uppfært

--- scripts/archive/testing_stanza_parser.py
+++ scripts/archive/testing_stanza_parser.py
@@ -6,9 +6,8 @@ nlp = stanza.Pipeline(
     constituency_model_path='/Users/esther/Documents/GitHub/stylometry-icelandiceval/models/is_icepahc_transformer_finetuned_constituency.pt'
 )
 headlines = [
-    "KPMG opnar skrifstofu á Blönduósi",
-    "Viltu taka þátt í vöruþróun ?",
-    "Íbúum Blönduóss og Skagastrandar fjölgar",
+    "Eg mun hér vera að eigi komist maðurinn út ef hann er hér inni en þú GAKK til stofu",
+    "'Gerðu annaðhvort,' sagði húskarl, 'að þú FAR á brott eða gakk inn og ver hér í nótt'" 
 ]
 
 for h in headlines:
@@ -17,21 +16,21 @@ for h in headlines:
     has_subject = 'NP*SBJ' in tree
     print(f"{'✓' if has_subject else '✗'} {h}")
 
-doc = nlp('Íbúum Blönduóss og Skagastrandar fjölgar')
+doc = nlp("Eg mun hér vera að eigi komist maðurinn út ef hann er hér inni en þú GAKK til stofu")
 print(str(doc.sentences[0].constituency))
 for sentence in doc.sentences:
     tree = str(sentence.constituency).replace('*', '-')
     tree = tree.replace('ROOT ', '')
     print(tree)
 
-doc = nlp('Viltu taka þátt í vöruþróun ?')
+doc = nlp("'Gerðu annaðhvort,' sagði húskarl, 'að þú FAR á brott eða gakk inn og ver hér í nótt'")
 print(str(doc.sentences[0].constituency))
 for sentence in doc.sentences:
     tree = str(sentence.constituency).replace('*', '-')
     tree = tree.replace('ROOT ', '')
     print(tree)
 
-doc = nlp('KPMG opnar skrifstofu á Blönduósi')
+doc = nlp('þú FAR á brott og gakk inn og ver hér í nótt')
 print(str(doc.sentences[0].constituency))
 for sentence in doc.sentences:
     tree = str(sentence.constituency).replace('*', '-')
